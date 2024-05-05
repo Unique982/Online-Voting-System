d8b5a82e3c4e41e6cc9c044481bd96684db84c4c
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-05-05 17:30:28 +0545

    Update register.php
    Update code

--- register.php
+++ register.php
@@ -1,64 +1,92 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Voting System</title>
-</head>
-<body>
-    <div class="container">
-        <div class="form">
-        <h1>Register From</h1>
-       <form action="Reg_From_Save.php" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <div class="input-box">
-            <label>Full Name:</label>
-            <input type="text" name="fullname" placeholder="Enter Your Full Name">
-            <span style="color: red;">*</span>
+  <head>
+    <title>
+    </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <style>
+    *{
+        margin: 0;
+        padding: 0;
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 18px;
+    }
+    
+  </style>
+  <body>
+  <div class="container">
+      <form action="Reg_From_Save.php"  method="POST" enctype="multipart/form-data" autocomplete="off">
+       <h1 style="text-align:center; font-size:30px; font-weight:bold; color:black;">Registration Form</h1>
+      <div class="row jumbotron"> 
+        <div class=" col-md-6">
+            <label>Full Name:</label>   <span style="color: red;">*</span>
+            <input type="text" name="fullname" class="form-control" placeholder="Enter Your Full Name">
         </div>
-       <div class="input-box">
-        <label>Username:</label>
-        <input type="text" name="username" placeholder="Enter Your Username">
-        <span style="color: red;">*</span>
+
+       <div class="col-md-6">
+        <label>Username:</label><span style="color: red;">*</span>
+        <input type="text" name="username" class="form-control" placeholder="Enter Your Username">
        </div>
-       <div class="input-box">
-        <label>Email:</label>
-        <input type="email" name="email" placeholder="Enter Your Email">
-        <span style="color: red;">*</span>
+     
+       <div class="col-md-6">
+        <label>Email:</label>   <span style="color: red;">*</span>
+        <input type="email" name="email" class="form-control" placeholder="Enter Your Email">
        </div>
-       <div class="input-box">
-        <label>Photo:</label>
-        <input type="file" name="image">
-        <span style="color: red;">*</span>
+
+       <div class="col-md-6">
+        <label>Phone Number:</label> <span style="color: red;">*</span>
+        <input type="number" name="phone" class="form-control" placeholder="Enter Your Number">
        </div>
-       <div class="input-box">
-        <label>Phone Number;</label>
-        <input type="number" name="phone">
-        <span style="color: red;">*</span>
+
+       <div class="col-md-6">
+        <label>Photo:</label>  <span style="color: red;">*</span>
+        <input type="file" name="image" class="form-control-file">
        </div>
-       <div class="input-box">
-        <label>Select:</label>
-        <select name="standard" id="">
+     
+       <div class="col-md-6">
+        <label>Select:</label>       <span style="color: red;">*</span>
+        <select name="standard" id="" class="form-control">
+        <option disabled selected>select one option</option>
             <option value="Group">Group</option>
             <option value="Voter">Voter</option>
         </select>
        </div>
-       <div class="input-box">
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Enter your Password">
-        <span style="color: red;">*</span>
+       <div class="col-md-6">
+        <label>Password</label> <span style="color: red;">*</span>
+        <input type="password" name="password" class="form-control" placeholder="Enter your Password">
+ 
        </div>
-       <div class="input-box">
-        <label>Confrom Password:</label>
-        <input type="password" name="cpassword" placeholder="Enter Your Confrom..">
-        <span style="color: red;">*</span>
+       <div class="col-md-6">
+        <label>Confrom Password:</label> <span style="color: red;">*</span>
+        <input type="password" class="form-control" name="cpassword" placeholder="Enter Your Confrom..">
+        
        </div>
-       <input type="submit" name="submit">
-       <div class="account">
-            <p>Already Have an account? <a href="login.html">Login</a></p>
-          </div>   
+       <div class="col-md-12" style="margin-top:20px; text-align:center">
+       <input type="submit" name="submit" class="btn btn-primary text-light">
+       </div>
+       <div class="col-md-12" style="text-align:center;">
+            <p>Already Have an account? <a href="login.php">Login</a></p>
+          </div>  
+          </div> 
     </form>
+               
+        
+      
     </div>
-    </div>
+
     
 </body>
+</html>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
